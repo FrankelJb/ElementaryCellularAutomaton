b950c45dfd5971f8691a6e23954249e3451aa40b
FrankelJb <jonathan@mint>
2013-09-20 09:46:20 +0200

    Modified directory structure, moved test file to src

--- src/CellularAutomataTest.py
+++ src/CellularAutomataTest.py
@@ -2,21 +2,21 @@
 
 __author__ = 'jonathan'
 import unittest
-from src.CellularAutomata import CellularAutomata
+from CellularAutomata import  CellularAutomata
 
 class TestCellularAutomata(unittest.TestCase):
 
     def test_buildgridpasses(self):
         cA = CellularAutomata()
-        cA.buildGrid('10')
-        self.assertRaises(RuntimeError, lambda: cA.buildGrid('101010110111111111111111111111111111111111'))
+        cA.buildGrid('10', 100)
+        self.assertRaises(RuntimeError, lambda: cA.buildGrid('101010110111111111111111111111111111111111' * 100, 100))
 
     #It is quite difficult to assert correctness, so I have chosen to use a row high up, row 3, and
     #ensure that it contains a substring that it should. The string used
     #was copied from http://www-inst.eecs.berkeley.edu/~selfpace/cs9honline/P1/
     def test_rowsarecorrect(self):
         cA = CellularAutomata()
-        grid = cA.buildGrid('1')
+        grid = cA.buildGrid('1', 100)
         cA.buildRules('30')
         grid = cA.mutate(grid, int(3))
         self.assertTrue('0110010' in '%s' % ''.join(map(str, grid[2])))
@@ -25,5 +25,5 @@ def main():
         unittest.main()
 
 if __name__ == '__main__':
-    main()
+    unittest.main()
 
